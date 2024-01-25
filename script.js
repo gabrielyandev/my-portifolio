353cb1b22698ecc766ba8a31c0c02c09fcb84c6e
Gabriel Yan <yanjob.dev@gmail.com>
2024-01-25 00:27:15 -0300

    update-animate

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function typeWrite(elemento){
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
-      
-    setTimeout(function(){
-        elemento.innerHTML += letra;
-    }, 75 * i)
-
-  });
+    if (elemento) {
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML = ' ';
+        textoArray.forEach(function(letra, i){   
+            setTimeout(function(){
+                elemento.innerHTML += letra;
+            }, 75 * i);
+        });
+    } else {
+        console.error('Elemento não encontrado');
+    }
 }
+
 const titulo = document.querySelector('.titulo-principal');
 typeWrite(titulo);
+
+/* reveal*/
+
+window.revelar = ScrollReveal({ reset: true });
+
+revelar.reveal('.hidden-element', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '90px',
+    delay: 100
+});
